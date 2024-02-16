a5806547bf53324d801b90c7dd942468ffadc748
phadung2546 <psb2546@gmail.com>
2024-02-16 10:29:20 +0700

    books

--- sever.js
+++ sever.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express()
-
+const books = require('./db.json')
 app.get('/' ,(req , res ) => {
     res.send("Hello World")
+})
+app.get('/book' , (req , res) =>{
+    res.json(books)
 })
 
 
